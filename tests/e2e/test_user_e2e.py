333c4b1d79cb00a5509bcb4f1e70c7a41534aca6
DSM6 <fatec-dsm6@academico.intra>
2026-05-11 22:06:36 -0300

    style: formata teste e2e com o black

--- tests/e2e/test_user_e2e.py
+++ tests/e2e/test_user_e2e.py
@@ -6,12 +6,14 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 
+
 def get_headless_driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     return webdriver.Chrome(options=options)
+
 
 def test_create_user_e2e():
     driver = get_headless_driver()
